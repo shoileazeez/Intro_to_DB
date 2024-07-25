2fca3c731322fd7800fd48ece7cc088a8dc967af
shoile azeez <shoabdulazeez@gmail.com>
2024-07-25 11:32:42 -0700

    my first sql file

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -1,18 +1,15 @@
-CREATE DATABASE IF NOT EXISTS alx_book_store
+CREATE DATABASE  alx_book_store;
+USE alx_book_store;
 
 CREATE TABLE Books (
     bookID INT PRIMARY KEY,
     title VARCHAR(130),
     author_id INT,
-    FOREIGN KEY (author_id) REFERENCES Authors_table(author_id),
+    -- FOREIGN KEY (author_id) REFERENCES Authors_table(author_id),
     price FLOAT,
      publication_date DATE
 );
 
-CREATE TABLE example_table (
-    id INT PRIMARY KEY,
-    value FLOAT
-);
 
 
 CREATE TABLE Authors(
@@ -39,7 +36,7 @@ CREATE TABLE Order_Details(
     order_details_id INT PRIMARY KEY,
     order_id INT,
     FOREIGN KEY (order_id) REFERENCES Orders(order_id),
-    book_id INT,
-    FOREIGN KEY (book_id) REFERENCES Books(book_id),
-    quantity DOUBLE
+    book_ID INT,
+    FOREIGN KEY (book_ID) REFERENCES Books(bookID),
+    quantity FLOAT
 );
