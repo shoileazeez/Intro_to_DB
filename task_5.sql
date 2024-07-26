0d9b11c12f908e5cd1a1c5972d74d3ae94413cf4
shoile azeez <shoabdulazeez@gmail.com>
2024-07-26 04:07:21 -0700

    my first database

---
+++ task_5.sql
@@ -0,0 +1,17 @@
+-- USE alx_book_store;
+-- SELECT 
+--     COLUMN_NAME, 
+--     DATA_TYPE, 
+--     IS_NULLABLE, 
+--     COLUMN_DEFAULT, 
+--     CHARACTER_MAXIMUM_LENGTH, 
+--     NUMERIC_PRECISION 
+-- FROM 
+--     INFORMATION_SCHEMA.COLUMNS 
+-- WHERE 
+--     TABLE_SCHEMA = 'alx_book_store' 
+--     AND TABLE_NAME = 'customers';
+
+INSERT INTO customers (customer_id, customer_name, email, address)
+VALUES 
+('1',  'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave');
