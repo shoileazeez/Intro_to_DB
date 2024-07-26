bc0e13a97a449e8adc8e35d15286c02ea0041e5a
shoile azeez <shoabdulazeez@gmail.com>
2024-07-26 04:08:49 -0700

    my first database

--- task_5.sql
+++ task_5.sql
@@ -14,4 +14,4 @@
 
 INSERT INTO customers (customer_id, customer_name, email, address)
 VALUES 
-('1',  'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave');
+('1',  'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.');
